faddbb006fe5b6042a6c5f1734319697db725e59
Ying Lau <501285598@qq.com>
2017-01-03 23:57:23 +0800

    add some file

--- routes/index.js
+++ routes/index.js
@@ -4,40 +4,57 @@ var router = express.Router();
 var EventProxy = require('eventproxy')
 var superagent = require('superagent');
 var cheerio = require('cheerio');
+var fs = require('fs');
+var path = require('path');
 
 router.get('/', function(req, res, next) {
-    console.log('开始抓取页面');
-    var eq = new EventProxy();
-    eq.all('postList', function(postList) {
-        console.log('数据已经获取');
-        postList.concat(postList);
-        console.log(postList);
-        //res.send();
-    });
-    for(var i = 1; i < 4; i++) {
-        console.log('开始抓取页面https://www.cnblogs.com/#p' + i);
-        superagent
-            .get('https://www.cnblogs.com/#p' + i)
-            .end(function(err, sres) {
-                if(err) {
-                    console.log('抓取出错');
-                    return next()
-                }
-                var data = [];
-                var $ = cheerio.load(sres.text);
-                var $cell = $('#post_list .post_item');
-                $cell.each(function(key, item) {
-                    data.push({
-                        link: $('.titlelnk', item).attr('href'),
-                        title: $('.titlelnk', item).text(),
-                        desc: $('.post_item_summary', item).text()
-                    })
-                });
-                eq.emit('postList', data);
-            });
-    }
+    var data = require('./data/data.json');
+    res.send(data[0])
+    // console.log('开始抓取页面');
+    // var ep = new EventProxy();
+    // // eq.all('postList', function(postList) {
+    // //     console.log('数据已经获取');
+    // //     postList.concat(postList);
+    // //     console.log(postList);
+    // //     //res.send();
+    // // });
+    // ep.after('postList', 3, function(data) {
+    //     console.log(data);
+    //     console.log('数据已经获取');
+    //     var out = fs.createWriteStream('./data/data.json')
+    //     data.map(function(item) {
+    //         out.write(JSON.stringify(item))
+    //     });
+    //
+    //     out.on('drain', function() {
+    //         console.log('数据已经全部输出!');
+    //     })
+    //
+    //     res.send(data[0]);
+    // });
+    // for(var i = 1; i < 4; i++) {
+    //     console.log('开始抓取页面https://www.cnblogs.com/#p' + i);
+    //     superagent
+    //         .get('https://www.cnblogs.com/#p' + i)
+    //         .end(function(err, sres) {
+    //             if(err) {
+    //                 console.log('抓取出错');
+    //                 return next()
+    //             }
+    //             var data = [];
+    //             var $ = cheerio.load(sres.text);
+    //             var $cell = $('#post_list .post_item');
+    //             $cell.each(function(key, item) {
+    //                 data.push({
+    //                     link: $('.titlelnk', item).attr('href'),
+    //                     title: $('.titlelnk', item).text(),
+    //                     desc: $('.post_item_summary', item).text()
+    //                 })
+    //             });
+    //             ep.emit('postList', data);
+    //         });
+    // }
 
-    console.log('end')
     //res.render('index')
 });
 
